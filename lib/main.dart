b3a7b10539e8fe818f598795109c09568c1d2008
rodrigo <rodrigo.rodriguez@live.com.ar>
2022-01-07 20:25:49 +0100

    search

--- lib/main.dart
+++ lib/main.dart
@@ -37,48 +37,52 @@ class cuerpo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Container(
-            margin: EdgeInsets.only(
-              top: size.height * 0.17,
-            ),
-            child: SafeArea(
-                child: GridView.builder(
-              gridDelegate:
-                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-              itemCount: items.length,
-              itemBuilder: (BuildContext context, int i) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => Detalle(ambo: items[i])));
-                  },
-                  child: Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(17)),
-                    child: Column(
-                      children: [
-                        Flexible(
-                            child: Image.asset('assets/img/juanita3p.jpg',
-                                height: size.height * .19)),
-                        Text(
-                          '${items[i].nombre}',
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Buscar'),
+        ),
+        body: Stack(
+          children: [
+            Container(
+                margin: EdgeInsets.only(
+                  top: size.height * 0.17,
+                ),
+                child: SafeArea(
+                    child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemCount: items.length,
+                  itemBuilder: (BuildContext context, int i) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Detalle(ambo: items[i])));
+                      },
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(17)),
+                        child: Column(
+                          children: [
+                            Flexible(
+                                child: Image.asset('assets/img/juanita3p.jpg',
+                                    height: size.height * .19)),
+                            Text(
+                              '${items[i].nombre}',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            Text('\$${items[i].precio}'),
+                          ],
                         ),
-                        Text('\$${items[i].precio}'),
-                      ],
-                    ),
-                    color: Colors.white,
-                    elevation: 10.0,
-                  ),
-                );
-              },
-            )))
-      ],
-    );
+                        color: Colors.white,
+                        elevation: 10.0,
+                      ),
+                    );
+                  },
+                )))
+          ],
+        ));
   }
 }
