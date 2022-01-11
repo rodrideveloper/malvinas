09a5cf9449cba5955e0b22a598f0c8a9302c4417
rodrigo <rodrigo.rodriguez@live.com.ar>
2022-01-11 00:54:08 -0300

    barraDeBusqueda agregada

--- lib/main.dart
+++ lib/main.dart
@@ -15,25 +15,182 @@ class Malvinas extends StatelessWidget {
   }
 }
 
-class cuerpo extends StatelessWidget {
-  final items = <Ambo>[
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-    Ambo('Leontina', 20, 'XS', 'XS'),
-  ];
+class cuerpo extends StatefulWidget {
+  const cuerpo({Key key}) : super(key: key);
 
+  @override
+  _cuerpoState createState() => _cuerpoState();
+}
+
+class _cuerpoState extends State<cuerpo> {
+  final TextEditingController _filtro = TextEditingController();
+  String _textoDeBusqueda = "";
+  List<Ambo> nombres = [];
+  List<Ambo> filtroNombres = [];
+  Icon _iconoBusqueda = const Icon(Icons.search);
+  Widget _appBarTitulo = const Text('Buscar...');
+
+  _cuerpoState() {
+    _filtro.addListener(() {
+      if (_filtro.text.isEmpty) {
+        setState(() {
+          _textoDeBusqueda = "";
+          filtroNombres = nombres;
+        });
+      } else {
+        setState(() {
+          _textoDeBusqueda = _filtro.text;
+        });
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    _getNames();
+    super.initState();
+  }
+
+  void _getNames() async {
+    /*  final response = await dio.get('https://swapi.co/api/people');
+    List tempList = [];
+    print(response.data);
+    print(response.data.length);
+    for (int i = 0; i < response.data['results'].length; i++) {
+      tempList.add(response.data['results'][i]);
+    }*/
+
+    List<Ambo> tempList = Ambo.getAmbos();
+    setState(() {
+      nombres = tempList;
+      nombres.shuffle();
+      filtroNombres = nombres;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(100),
+        child: _buildBar(context),
+      ),
+      body: Container(
+        child: _buildList(context),
+      ),
+      resizeToAvoidBottomInset: false,
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      title: _appBarTitulo,
+      leading: IconButton(
+        icon: _iconoBusqueda,
+        onPressed: _searchPressed,
+      ),
+    );
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (_iconoBusqueda.icon == Icons.search) {
+        _iconoBusqueda = Icon(Icons.close);
+        _appBarTitulo = TextField(
+          controller: _filtro,
+          decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.search), hintText: 'Buscar...'),
+          autofocus: true,
+          style: TextStyle(color: Colors.white),
+        );
+      } else {
+        this._iconoBusqueda = Icon(Icons.search);
+        this._appBarTitulo = Text('Buscar...');
+        filtroNombres = nombres;
+        _filtro.clear();
+      }
+    });
+  }
+
+  Widget _buildList(context) {
+    final size = MediaQuery.of(context).size;
+    if (!(_textoDeBusqueda.isEmpty)) {
+      List<Ambo> tempList = [];
+      for (int i = 0; i < filtroNombres.length; i++) {
+        if (filtroNombres[i]
+            .nombre
+            .toLowerCase()
+            .contains(_textoDeBusqueda.toLowerCase())) {
+          tempList.add(filtroNombres[i]);
+        }
+      }
+      filtroNombres = tempList;
+    }
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      itemCount: filtroNombres.length,
+      itemBuilder: (BuildContext context, int i) {
+        return GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => Detalle(ambo: filtroNombres[i])));
+          },
+          child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(17)),
+            child: Column(
+              children: [
+                Flexible(child: filtroNombres[i].image),
+                Text(
+                  '${filtroNombres[i].nombre}',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text('\$${filtroNombres[i].precio}'),
+              ],
+            ),
+            color: Colors.white,
+            elevation: 10.0,
+          ),
+        );
+      },
+    );
+
+    /*ListView.builder(
+      itemCount: names == null ? 0 : filteredNames.length,
+      itemBuilder: (BuildContext context, int index) {
+        return ListTile(
+          title: Text(filteredNames[index].nombre),
+          /* onTap: () => print(filteredNames[index]['name']),*/
+        );
+      },
+    );*/
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class cuerpo extends StatelessWidget {
+  List items = Ambo.getAmbos();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -86,3 +243,4 @@ class cuerpo extends StatelessWidget {
         ));
   }
 }
+*/
--- lib/models/ambo.dart
+++ lib/models/ambo.dart
@@ -1,8 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
 class Ambo {
   String nombre;
   int precio;
   String talleChaqueta;
   String tallePantalon;
+  Image image;
 
-  Ambo(this.nombre, this.precio, this.talleChaqueta, this.tallePantalon);
+  Ambo(this.nombre, this.precio, this.talleChaqueta, this.tallePantalon,
+      this.image);
+
+  static List<Ambo> getAmbos() {
+    final items = <Ambo>[
+      Ambo('Leontina', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Blue', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Femme', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Juanita', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Lucrecia', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('3 piezas', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Arciel', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Batista', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Caramelo', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Fuerza', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Leon 2', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+      Ambo('Hombre', 20, 'XS', 'XS', Image.asset('assets/img/juanita3p.jpg')),
+    ];
+    return items;
+  }
 }
